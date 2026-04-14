5ab6ad9cec1af298dd4d4966b07d6aa64753229a
awinarko <andy.winarko@paprika.co.id>
2026-04-14 23:22:47 +0700

    feat: add app layout, calculator page, and route

---
+++ resources/views/calculator.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="min-h-screen flex items-center justify-center p-4">
+        <livewire:calculator />
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calculator</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body class="bg-gray-100 dark:bg-gray-900 min-h-screen">
+    @yield('content')
+    @livewireScripts
+</body>
+</html>
